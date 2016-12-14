42c7f15818547520e1317d96701ba2f5de3a27ee
sleaz0id <mat.staszczyk@gmail.com>
2016-12-14 22:53:39 +0100

    Implement basic login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,4 @@
 module SessionsHelper
-
   # Logs in the given user.
   def log_in(user)
     session[:user_id] = user.id
@@ -13,5 +12,10 @@ module SessionsHelper
   # Returns true if the user is logged in, false otherwise
   def logged_in?
     !current_user.nil?
+  end
+  # Logs out the current user.
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
   end
 end
