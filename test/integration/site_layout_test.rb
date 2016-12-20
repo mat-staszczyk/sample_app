50136fb1bea2c48bd47f23bda2315e8012361913
sleaz0id <mat.staszczyk@gmail.com>
2016-12-20 21:13:52 +0100

    Some tests added

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,9 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
 
   test 'layout links' do
     get root_path
@@ -13,5 +16,15 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", login_path
     get contact_path
     assert_select "title", full_title("Contact")
+  end
+
+  test 'layout links for logged in user' do
+    log_in_as(@user)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
   end
 end
