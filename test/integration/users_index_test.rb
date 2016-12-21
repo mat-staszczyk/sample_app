f2a1d002d2c6818a65b1e8eb636d7a2d18995ca9
sleaz0id <mat.staszczyk@gmail.com>
2016-12-21 22:38:14 +0100

    Finish user edit, update, index, and destroy actions

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class UsersIndexTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
+    @non_admin = users(:archer)
   end
 
   test 'index including pagination' do
@@ -15,4 +16,11 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
       assert_select 'a[href=?]', user_path(user), text: user.name
     end
   end
+
+  test 'index as non-admin' do
+    log_in_as(@non_admin)
+    get users_path
+    assert_select 'a', text: 'delete', count: 0
+  end
+
 end
