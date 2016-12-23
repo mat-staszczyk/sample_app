297d2c3ba3044424408949a6ff1beb3b2e45ce2b
sleaz0id <mat.staszczyk@gmail.com>
2016-12-23 19:02:58 +0100

    Generalize the auth method

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,9 +28,10 @@ class User < ApplicationRecord
   end
 
   # Retruns true if the given token matches the digest
-  def authenticated?(remember_token)
-    return false unless remember_digest
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
   end
 
   # Forgets a user
