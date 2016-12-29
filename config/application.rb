9ef12e039ed9675351163dbd1bdabfccb6da7acc
sleaz0id <mat.staszczyk@gmail.com>
2016-12-29 15:56:29 +0100

    Timezone fixed

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,6 @@ module SampleApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.time_zone = 'Warsaw'
+    config.time_zone = 'Europe/Warsaw'
   end
 end
