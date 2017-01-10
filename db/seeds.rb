eae4e687270baf149ae8b9a7d75e267fa730189f
sleaz0id <mat.staszczyk@gmail.com>
2017-01-10 19:29:18 +0100

    Following relationship stup

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
+# Users
 User.create!(name:  'Falka',
              email: 'sleazoid@o2.pl',
              password:                'password',
@@ -34,8 +27,17 @@ User.create!(name:  'Example User',
                activated_at: Time.zone.now)
 end
 
+# Microposts
 users = User.order(:created_at).take(6)
 50.times do
   content = Faker::Hipster.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+# Following relationships
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
